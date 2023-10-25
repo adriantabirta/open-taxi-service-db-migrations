77fa7a368529ee76b74775d8f98937b1b8c0c98f
at <adriantabirta@gmail.com>
2023-10-25 13:52:36 +0300

    added transactions

--- 1_initialize_schema.up.sql
+++ 1_initialize_schema.up.sql
@@ -38,3 +38,12 @@ CREATE TABLE IF NOT EXISTS locations (
     latitude DOUBLE PRECISION NOT NULL,  
     timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id BIGSERIAL PRIMARY KEY,
+    order_id BIGINT NOT NULL,
+    payment_method text NOT NULL DEFAULT '',
+    amount INTEGER NOT NULL,  
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now(),
+);
