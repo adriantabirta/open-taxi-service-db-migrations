cadaf9fdc4cb9828521f2ce1cdf6649597632994
at <adriantabirta@gmail.com>
2023-10-25 15:21:13 +0300

    updated transactions

--- 1_initialize_schema.up.sql
+++ 1_initialize_schema.up.sql
@@ -42,8 +42,10 @@ CREATE TABLE IF NOT EXISTS locations (
 CREATE TABLE IF NOT EXISTS transactions (
     id BIGSERIAL PRIMARY KEY,
     order_id BIGINT NOT NULL,
-    payment_method text NOT NULL DEFAULT '',
+    payment_method INTEGER NOT NULL,
     amount INTEGER NOT NULL,  
     created_at timestamp without time zone NOT NULL DEFAULT now(),
     updated_at timestamp without time zone NOT NULL DEFAULT now(),
+
+    CONSTRAINT fk_transactions_orders FOREIGN KEY(order_id) REFERENCES orders(id)
 );
